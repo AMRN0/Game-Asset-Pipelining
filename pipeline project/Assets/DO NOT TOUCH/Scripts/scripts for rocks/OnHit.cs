9e6cebd7c1daf07c5b23aff779bc0bc9ba40a3dc
AMRN0 <ali.noorani@mail.bcu.ac.uk>
2021-05-23 20:46:52 +0100

    final commit

---
+++ pipeline project/Assets/DO NOT TOUCH/Scripts/scripts for rocks/OnHit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHit : MonoBehaviour
+{
+    public Object PS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter()
+    {
+        Instantiate(PS, gameObject.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+
+    }
+}
