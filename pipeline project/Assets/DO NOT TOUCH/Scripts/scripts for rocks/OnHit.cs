4a3a89fb8441c6fe59e342937189831aa280908e
AMRN0 <ali.noorani@mail.bcu.ac.uk>
2021-05-24 14:50:35 +0100

    screenshots taken for the final video

--- pipeline project/Assets/DO NOT TOUCH/Scripts/scripts for rocks/OnHit.cs
+++ pipeline project/Assets/DO NOT TOUCH/Scripts/scripts for rocks/OnHit.cs
@@ -5,18 +5,6 @@ using UnityEngine;
 public class OnHit : MonoBehaviour
 {
     public Object PS;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnBecameInvisible()
     {
         Destroy(gameObject);
@@ -26,6 +14,5 @@ public class OnHit : MonoBehaviour
     {
         Instantiate(PS, gameObject.transform.position, Quaternion.identity);
         Destroy(gameObject);
-
     }
 }
